430d8c6de6c3ea18a50bfa8eda9991ce1ec42f3c
Dave Renz <david.renz@gmail.com>
2012-12-07 11:34:45 -0500

    added a route to allow you log out of the app with get /users/sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Bedside::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
-  devise_for :users
+  
+  devise_for :users do 
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
+  
   resources :users
   resources :pages do
     resources :wishes, :posts, :page_participations, :visits, :comments
