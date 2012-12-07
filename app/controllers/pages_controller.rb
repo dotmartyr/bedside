bd9b5387182a29f326edca91acef1552c6cc8d35
Dave Renz <david.renz@gmail.com>
2012-12-07 11:51:10 -0500

    initial version of pages controller, just for quick testing. needs refactored.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,29 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
-
+  respond_to :html, :js
+  
   def show
-    @page = Page.find_by_id(params[:id])
+    @page = Page.find(params[:id])
+
+    respond_with(@page)
   end
 
-  def create
+  def new
+    respond_with(@page = Page.new)   
+  end 
 
+  def create
+    @page = Page.new(params[:page])
+  
+    respond_with(@page)
   end
 
   def update
-  end
+    @page = Page.find(params[:id])
 
-  def destroy
+    @page.update_attributes(params[:page])
+    
+    respond_with(@page)
   end
 
 end
