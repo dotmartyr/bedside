24532db20639bf12add02dc58a5ea3fdf3d44ae4
Dave Renz <david.renz@gmail.com>
2012-12-08 15:38:41 -0500

    created helper method on page to identify whether a the current page can be edited by the given user

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,4 +7,10 @@ class Page < ActiveRecord::Base
   has_many :visits
   has_many :wishes
 
+  def can_edit?(user)
+    pp = user.page_participations.find_by_page_id(self.id)
+
+    !pp.nil? && (pp.permission_level == PageParticipation::PermissionLevel::OWNER || pp.permission_level == PageParticipation::PermissionLevel::PATIENT)
+  end
+
 end
