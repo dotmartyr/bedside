6320437918c2cefca5053053d63b485ee88df422
Dave Renz <david.renz@gmail.com>
2012-12-08 12:41:50 -0500

    fixed minor bug.  the home controller was trying to look the pages of non-logged in users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     # If the user doesn't have any pages yet, redirect to the page creation screen
-    redirect_to '/pages/new' if current_user.pages.blank?
+    redirect_to '/pages/new' if current_user && current_user.pages.blank?
     # If the user has pages, redirect to the the most recent
     # TODO: order this properly to ensure it's the most recent
     redirect_to current_user.pages.first if current_user && current_user.pages.exists?
