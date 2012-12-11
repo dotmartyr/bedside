982b0177e106bf441cf24e2c445f1d272718c5a4
Dave Renz <david.renz@gmail.com>
2012-12-11 13:08:04 -0500

    Added settings modal for editing a user's profile.  Still need to add email subscription options.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,38 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-
+  respond_to :html, :js
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
     @users = User.all
+    respond_with(@users)
   end
 
   def show
     @user = User.find(params[:id])
+    respond_with(@user)
   end
   
+  def edit
+    respond_with(@user = current_user)
+  end
+
+
   def update
-    authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user], :as => :admin)
-      redirect_to users_path, :notice => "User updated."
-    else
-      redirect_to users_path, :alert => "Unable to update user."
+    
+
+    if params[:user][:password].blank? 
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
     end
+    
+    if @user.update_attributes(params[:user])
+      #prevent the user from being logged out by 
+      #changing their password
+      sign_in(@user, :bypass => true)
+    end
+
+    respond_with(@user)
   end
     
   def destroy
