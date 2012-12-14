a36a2eab22bac7db37c346093ce929f4c5510aa7
Dave Renz <david.renz@gmail.com>
2012-12-13 23:36:11 -0500

    Found another redis issue…

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,8 +2,8 @@ require 'resque'
 require 'resque/server'
 require 'yaml'
 
-resque_config = YAML.load_file(Rails.root + 'config/redis.yml')
-Resque.redis = resque_config[Rails.env]
+#resque_config = YAML.load_file(Rails.root + 'config/redis.yml')
+#Resque.redis = resque_config[Rails.env]
 
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
 
