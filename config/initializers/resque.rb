6f60f00eb92cbb72a2f3d687477bad0c11cd2665
Eric Skiff <me@ericskiff.com>
2012-12-20 11:19:35 -0500

    configure redis for redistogo

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,6 +4,10 @@ require 'yaml'
 
 #resque_config = YAML.load_file(Rails.root + 'config/redis.yml')
 #Resque.redis = resque_config[Rails.env]
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+end
 
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
 
