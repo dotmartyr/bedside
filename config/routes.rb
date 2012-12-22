95ba7f200fbf0f7d49c3188bc0c6b4cfe1873957
Robert Beene <rbeene@teladoc.com>
2012-12-22 11:33:52 -0500

    Remove invalid routes
    Visits paths are not valid as the visits controller expects to be a
    nested route under page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
 Bedside::Application.routes.draw do
 
-  get "visits/show"
-
-  get "visits/edit"
-
-  get "visits/new"
-
-  get "visits/create"
-
-  get "visits/update"
-
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
-  
-  devise_for :users do 
-    get '/users/sign_out' => 'devise/sessions#destroy' 
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
+
   resources :users
   resources :pages do
     resources :wishes, :posts, :page_participations, :visits, :comments
